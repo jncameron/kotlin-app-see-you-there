9e736313b099ec70f67fb192e78d7933fb0c3078
john <john.n.cameron@gmail.com>
2018-05-16 15:06:29 +1000

    myEvents ui completed

--- app/src/main/java/com/example/johncameron/seeyouthere/adapters/MyEventsRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/johncameron/seeyouthere/adapters/MyEventsRecyclerViewAdapter.kt
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.example.johncameron.seeyouthere.R
@@ -20,12 +21,13 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DatabaseReference
+import com.squareup.picasso.Picasso
 
 
 class MyEventsRecyclerViewAdapter(databaseQuery: DatabaseReference, var context: Context)
     :FirebaseRecyclerAdapter<Events, MyEventsRecyclerViewAdapter.ViewHolder>(
         Events::class.java,
-        R.layout.fragment_my_events,
+        R.layout.fragment_my_events2,
         MyEventsRecyclerViewAdapter.ViewHolder::class.java,
         databaseQuery){
 
@@ -37,7 +39,7 @@ class MyEventsRecyclerViewAdapter(databaseQuery: DatabaseReference, var context:
 
     override fun populateViewHolder(myEventsViewHolder: ViewHolder?, event: Events?, position: Int) {
         var eventId = getRef(position).key
-
+        
 
         mCurrentUser = FirebaseAuth.getInstance().currentUser
 
@@ -45,7 +47,6 @@ class MyEventsRecyclerViewAdapter(databaseQuery: DatabaseReference, var context:
 
 
             myEventsViewHolder!!.bindView(event!!, context)
-
             myEventsViewHolder.itemView.setOnClickListener {
 
 
@@ -70,24 +71,24 @@ class MyEventsRecyclerViewAdapter(databaseQuery: DatabaseReference, var context:
         fun bindView(event: Events, context: Context) {
 
             var eventName = itemView.findViewById<TextView>(R.id.myEventTitle)
-            var eventHost = itemView.findViewById<TextView>(R.id.myEventHostedBy)
             var eventLocation = itemView.findViewById<TextView>(R.id.myEventLocation)
-            var eventDetails = itemView.findViewById<TextView>(R.id.myEventDetails)
-            var eventBring = itemView.findViewById<TextView>(R.id.myEventBring)
             var eventDate = itemView.findViewById<TextView>(R.id.myEventDate)
             var eventTime = itemView.findViewById<TextView>(R.id.myEventTime)
-            var attendees = itemView.findViewById<TextView>(R.id.myAttending)
+            var attendees = itemView.findViewById<TextView>(R.id.attending)
 
 
 
             eventName.text = event.eventName
-            eventHost.text = event.eventHost
             eventLocation.text = event.eventLocation
-            eventDetails.text = event.eventDetails
-            eventBring.text = event.eventBring
-            eventDate.text = event.eventDate
+
+            var eventDateFormatted = event.eventDate
+            eventDateFormatted = eventDateFormatted!!.replaceFirst("\\s".toRegex(), "\n")
+
+
+            eventDate.text = eventDateFormatted
             eventTime.text = event.eventTime
             attendees.text = event.attending?.size.toString()
+            Picasso.with(context).load(event.eventImage).into(itemView.findViewById<ImageView>(R.id.myEventImage))
 
 
 
